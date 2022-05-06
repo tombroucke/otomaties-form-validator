c17f587cd3b9c9cf272ce7905594ed6b99289f54
Tom Broucke <tom@tombroucke.be>
2022-05-06 15:52:09 +0200

    parse input number as floats

--- inputs/input-number.js
+++ inputs/input-number.js
@@ -3,19 +3,23 @@ import SimpleInput from "./simple-input";
 export default class InputNumber extends SimpleInput {
 	errors(errors = {}) {
 		// Check max
-		const max = this.el.getAttribute('max');
+		const max = parseFloat(this.el.getAttribute('max'));
 		if(max && this.val() && this.val() > max) {
 			let errorMessage = this.polyglot.t('Enter a value less than or equal to {0}');
 			errors.max = errorMessage.replace('{0}', max);
 		}
 
 		// Check min
-		const min = this.el.getAttribute('min');
+		const min = parseFloat(this.el.getAttribute('min'));
 		if(min && this.val() && this.val() < min) {
 			let errorMessage = this.polyglot.t('Enter a value greater than or equal to {0}');
 			errors.min = errorMessage.replace('{0}', min);
 		}
 
 		return super.errors(errors);
+	}
+
+	val() {
+		return parseFloat(this.el.value);
 	}
 }
