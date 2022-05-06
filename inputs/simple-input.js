12b432c78f399d0f766cb9bb030cf77f04df8f73
Tom Broucke <tom@tombroucke.be>
2022-05-06 15:28:47 +0200

    Better required field description

--- inputs/simple-input.js
+++ inputs/simple-input.js
@@ -3,7 +3,7 @@ import Input from "./input";
 export default class SimpleInput extends Input {
 	errors(errors = {}) {
 		if(this.isRequired() && this.el.value.length == 0) {
-			errors.required = this.polyglot.t('Field is required');
+			errors.required = this.polyglot.t('This field is required');
 		}
 		return super.errors(errors);
 	}
