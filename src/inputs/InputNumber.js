8594a9d18a7a3aea95093f0cb35e382d3e4a2a07
Tom Broucke <tom@tombroucke.be>
2022-12-19 21:45:18 +0100

    fix(Number): fix for required number field. only check min and max when input value is numeric

--- src/inputs/InputNumber.js
+++ src/inputs/InputNumber.js
@@ -2,24 +2,24 @@ import SimpleInput from "./SimpleInput";
 
 export default class InputNumber extends SimpleInput {
 	errors(errors = {}) {
-		// Check max
-		const max = parseFloat(this.el.getAttribute('max'));
-		if(max && this.val() && this.val() > max) {
-			let errorMessage = this.errorMessage('max', this.polyglot.t('Enter a value less than or equal to {0}'));
-			errors.max = errorMessage.replace('{0}', max);
-		}
-
-		// Check min
-		const min = parseFloat(this.el.getAttribute('min'));
-		if(min && this.val() && this.val() < min) {
-			let errorMessage = this.errorMessage('min', this.polyglot.t('Enter a value greater than or equal to {0}'));
-			errors.min = errorMessage.replace('{0}', min);
+		if (!isNaN(this.el.value)) {
+			const floatValue = parseFloat(this.el.value);
+	
+			// Check max
+			const max = parseFloat(this.el.getAttribute('max'));
+			if(max && floatValue > max) {
+				let errorMessage = this.errorMessage('max', this.polyglot.t('Enter a value less than or equal to {0}'));
+				errors.max = errorMessage.replace('{0}', max);
+			}
+	
+			// Check min
+			const min = parseFloat(this.el.getAttribute('min'));
+			if(min && floatValue < min) {
+				let errorMessage = this.errorMessage('min', this.polyglot.t('Enter a value greater than or equal to {0}'));
+				errors.min = errorMessage.replace('{0}', min);
+			}
 		}
 
 		return super.errors(errors);
-	}
-
-	val() {
-		return parseFloat(this.el.value);
 	}
 }
